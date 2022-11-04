a7dc32412cf05c21d318ff5d210ab9db2f5b8637
Zoey Neale <zn4@rice.edu>
2022-11-04 13:37:12 -0500

    Changes from local computer

---
+++ comp pred temp analysis.R
@@ -0,0 +1,16 @@
+---
+#'title: "comp pred temp analysis"
+#'author: "Zoey Neale"
+#'date: "2022-11-04"
+#'output:
+#'  html_document:
+#'  keep_md: yes
+---
+
+#+{r setup, include=FALSE
+knitr::opts_chunk$set(echo = TRUE)
+
+
+#+{r
+library(tidyverse)
+
