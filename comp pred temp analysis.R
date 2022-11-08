14a090fad9c9bc3791cacab09b717b378e21445d
Zoey Neale <zn4@rice.edu>
2022-11-08 16:38:21 -0600

    Change from my local computer

--- comp pred temp analysis.R
+++ comp pred temp analysis.R
@@ -82,14 +82,11 @@ Pulexpredict <- predict(Pulexglmer, newdata = Pulexdf, re.form = NA, se.fit = T,
 
 ###Create a data frame out of the fit and CI lists. They'll need to be transposed to bind to Pulexdf
 
-x<-Pulexpredict$fit%>%
-  data.frame()
 
 Pulexdf <- Pulexdf%>%
   mutate(fit = unname(data.frame(Pulexpredict$fit)$Pulexpredict.fit),
          lwr.CI = unname(t(data.frame(Pulexpredict$ci.fit))[,1]),
          upr.CI = unname(t(data.frame(Pulexpredict$ci.fit))[,2]))
-
 
 
 #Plot
@@ -123,18 +120,81 @@ library(merTools)
 
 predictInterval(Pulexglmer, newdata = Pulexdf, which = "fixed", include.resid.var = F)
 
+
+########################################
+
 #Simo
 Simodf <- df%>%
   filter(Species %in% c("Simo", "Comp"))%>%
-  drop_na(Simo) #Adding this because at the moment all the samples haven't been processed. I need to check any NA's after all data has be input
+  drop_na(Simo.surv) #Adding this because at the moment all the samples haven't been processed. I need to check any NA's after all data has be input
 
 ##Exploratory analysis
 Simodf%>%
-  ggplot(aes(x = TempC, y = Simo, color = Pred)) +
+  ggplot(aes(x = TempC, y = Simo.surv, color = Pred)) +
   geom_point() +
   facet_wrap(~Species) +
   theme_classic()
 
+Simodf%>%
+  ggplot(aes(x = Simo.surv)) +
+  geom_histogram()
+
+##Check out the distribution
+
+descdist(Simodf$Simo.surv)
+
+#Mixed effects
+
+library(lme4)
+Simoglmer <- Simodf%>%
+  glmer(Simo.surv ~ poly(TempC, 2) * Species * Pred + (1|Trial) + (1|Bath),
+        family = "poisson", data = .)
+
+plot(Simoglmer)
+
+Anova(Simoglmer, type = 3)
+
+#Generate predicted values and CI
+
+##Trying bootpredictlme4 package
+
+
+###Calculate the predicted values and CI by bootstrapping. Going with 10 simulations for now, but I'll need to increase that.
+
+Simopredict <- predict(Simoglmer, newdata = Simodf, re.form = NA, se.fit = T, nsim = 10, type = "response")
+
+###Create a data frame out of the fit and CI lists. They'll need to be transposed to bind to Pulexdf
+
+
+
+Simodf <- Simodf%>%
+  mutate(fit = unname(data.frame(Simopredict$fit)$Simopredict.fit),
+         lwr.CI = unname(t(data.frame(Simopredict$ci.fit))[,1]),
+         upr.CI = unname(t(data.frame(Simopredict$ci.fit))[,2]))
+
+
+
+#Plot
+
+##Facet by comp
+
+Simodf%>%
+  ggplot(aes(x = TempC, y = Simo.surv, color = Pred)) +
+  geom_point() +
+  geom_line(aes(y = fit)) +
+  geom_ribbon(aes(ymin = lwr.CI, ymax = upr.CI, fill= Pred), alpha = 0.5, color = NA) +
+  facet_wrap(vars(Species)) +
+  theme_classic()
+
+##Facet by pred
+
+Simodf%>%
+  ggplot(aes(x = TempC, y = Simo.surv, color = Species)) +
+  geom_point() +
+  geom_line(aes(y = fit)) +
+  geom_ribbon(aes(ymin = lwr.CI, ymax = upr.CI, fill= Species), alpha = 0.5, color = NA) +
+  facet_wrap(vars(Pred)) +
+  theme_classic()
 
 
 
